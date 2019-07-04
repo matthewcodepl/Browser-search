20cd333f1b1b9f519f63a908e603a663b3bd45aa
matthewcodepl <matthewcodepl@DESKTOP-EU96CO9.localdomain>
2019-07-04 17:25:51 +0200

    upgrade js code, add keycode

--- main.js
+++ main.js
@@ -1,57 +1,55 @@
 /*
-- Strona zmienia motyw w zależności od godziny
-- Ustawiony zegar na stronie
+- Strona zmienia motyw w zależności od pory dnia 
+- Ustawiony zegar na stronie -- ZROBIONE -- 
 -znaleźć ładną grafikę/grafiki
+-dodać cytaty 
+- ładnie ostylować stronę
 */
 
 let defaultSearch = "http://google.com/search?q=";
 let input = document.querySelector(".inp");
+input.addEventListener('keydown', e => {
+    let keycode = e.keyCode;
+    if (keycode == 13) {
 
-// let btnLink = document.querySelector('#link');
+        function search() {
+            window.open(`${defaultSearch}${q}`);
 
-function search() {
-    window.open(`${defaultSearch}${q}`);
+          }
+          
+          if (input.value > 0) {
+            input.addEventListener("input", search, false);
 
-}
+          }
 
-
-if(input.value > 0) {
-    btn.addEventListener('input', search, false);
-
-}
-
-
-
+             
+                setTimeout(() => {
+                        input.value = '';
+                }, 1);
+    }
+  });
+  
 
 
 function showClock() {
     /*
         Get the time from object of Date
     */
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    const header = document.querySelector(".top");
-        if ( hours > 24) {
-        hours = 01
-
-    } 
-
-    hours < 10 ?  hours =  `0 ${hours}` : hours;
-    minutes < 10 ? minutes = `0 ${minutes}` : minutes;
-    seconds < 10 ? seconds = `0${seconds}`: seconds;
-
-    let time = `${hours} : ${minutes} : ${seconds}`;
-
-
-   document.querySelector("#clock").textContent = time;
-
-   
-
-
-    setTimeout(showClock, 1000);
-
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  const header = document.querySelector(".top");
+  if (hours > 24) {
+    hours = 01;
+  }
+  hours < 10 ? (hours = `0${hours}`) : hours;
+  minutes < 10 ? (minutes = `0${minutes}`) : minutes;
+  seconds < 10 ? (seconds = `0${seconds}`) : seconds;
+  let time = `${hours} : ${minutes} : ${seconds}`;
+  document.querySelector("#clock").textContent = time;
+  setTimeout(showClock, 1000);
 }
 
 showClock();
+
